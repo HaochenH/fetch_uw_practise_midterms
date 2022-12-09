2990fca6af9fde1c005e3cba241aecaeb3a829fc
Haochen Hu <hhuuucc@Hotmail.com>
2022-12-08 16:09:50 -0800

    Add support for terminal arguments as url

--- fetch_uw_practise_midterms.py
+++ fetch_uw_practise_midterms.py
@@ -1,47 +1,56 @@
 import httpx
 import lxml.html
-from rich import print
-import re
+import sys
 import platform
 import os
 
-# url = "https://sites.math.washington.edu/~m208/Midterm1.php"
-url = input("Input url: ")
+if len(sys.argv) > 1: # accept url as terminal parameter
+    print('URL Received: {}'.format(sys.argv[1]))
+    url = sys.argv[1]
+else:
+    url = input("Input url here\n(default: https://sites.math.washington.edu/~m208/Midterm1.php):\n>>> ")
+if not url or not url.startswith('http'):
+    url = "https://sites.math.washington.edu/~m208/Midterm1.php"
 
-parent_output_dir = os.path.expanduser('~/Downloads')
-p = os.path.dirname(url).split('/')[-1].replace('~', '')
-output_dir = os.path.join(parent_output_dir, p)
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
-    print('{} created'.format(output_dir))
+def output_dir(url):
+    parent_output_dir = os.getcwd() # current working directory
+    p = os.path.dirname(url).split('/')[-1].replace('~', '')
+    output_dir = os.path.join(parent_output_dir, p)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+        print('{} created'.format(output_dir))
+    return output_dir
 
-res = httpx.get(url)
+def find_links(url):
+    res = httpx.get(url)
+    tree = lxml.html.fromstring(res.text) # convert html string into an etree
+    url_path = os.path.dirname(url)
+    print(f'URL path: {url_path}')
+    print(f'Text found: {tree.xpath("//table//a/text()")}')
+    links = [url_path + l[1:] for l in tree.xpath('//table//a/@href')]
+    return links
 
-# convert html string into an etree
-tree = lxml.html.fromstring(res.text)
-print(type(tree)) # <class 'lxml.html.HtmlElement'>
+def save_files(links, output_dir):
+    local_paths = []
+    for link in links:
+        res = httpx.get(link)
+        filename = link.split('/')[-1]
+        filepath = os.path.join(output_dir, filename)
+        with open(filepath, 'wb') as f:
+            size = f.write(res.content)
+            print('{} bytes written to "{}"'.format(size, filepath))
+        local_paths.append(filepath)
+    return local_paths
 
-# base_url = re.findall(r'(.*?:\/\/.*?)\/', url)[0]
-url_path = os.path.dirname(url)
-print(url_path)
-
-a_texts = tree.xpath('//table//a/text()')
-links = [url_path + l[1:] for l in tree.xpath('//table//a/@href')]
-print(f'A text found: {a_texts}')
-print('links found:')
-print(links)
-
-lcoal_paths = []
-for link in links:
-    res = httpx.get(link)
-    filename = link.split('/')[-1]
-    filepath = os.path.join(output_dir, filename)
-    with open(filepath, 'wb') as f:
-        size = f.write(res.content)
-        print("{} bytes written to \"{}\"".format(size, filepath))
-    lcoal_paths.append(filepath)
-
-if platform.system() == 'Darwin':
-    os.system('open "{}"'.format(output_dir))
-elif platform.system() == 'Windows':
-    os.system('explorer "{}"'.format(output_dir))
+def open_dir(path):
+    if platform.system() == 'Darwin':
+        os.system('open "{}"'.format(path))
+    elif platform.system() == 'Windows':
+        os.system('explorer "{}"'.format(path))
+    elif platform.system() == 'Linux':
+        os.system('xdg-open "{}"'.format(path))
+        
+output_dir = output_dir(url)
+links = find_links(url)
+save_files(links, output_dir)
+open_dir(output_dir)
